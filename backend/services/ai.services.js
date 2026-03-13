9bde584859630907c5d20f5467f8b9f004cc904d
NieshKumar <nileshkumar200518@gmail.com>
2026-03-14 00:04:56 +0530

    chanege the model

--- backend/services/ai.services.js
+++ backend/services/ai.services.js
@@ -15,7 +15,7 @@ const explainError = async (error, language) => {
     try {
 
         const response = await ai.models.generateContent({
-            model: "gemini-1.5-flash",
+            model: "gemini-2.5-flash",
             contents: `
         You are a senior software engineer and debugging expert helping developers fix errors quickly and learn from them.
 
